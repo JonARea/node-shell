27f2106658149b641342fe4ef6f7a7c6e8b2b011
Jon Rea <jonarea@gmail.com>
2017-11-06 12:29:32 -0500

    pwd and date working

--- bash.js
+++ bash.js
@@ -5,9 +5,11 @@ process.stdout.write('prompt > ');
 // The stdin 'data' event fires after a user types in a line
 
 process.stdin.on('data', function (input) {
-  console.log(input.toString().toLowerCase());
-  if (input.toString().toLowerCase() == 'pwd\n') {
-    return process.stdout.write(process.env.PWD);
+  if (input.toString() === 'pwd\n') {
+    process.stdout.write(process.env.PWD);
+  } else if (input.toString().trim() === 'date') {
+    let date = new Date()
+    process.stdout.write(date.toUTCString())
   }
 
   process.stdout.write('\nprompt > ');
